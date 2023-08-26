3610433aacbbb5763bc268f2da3d32de72dd9505
Maro Ridhiwani <maroridhiwani@gmail.com>
2023-08-26 09:25:37 +0300

    my fouuth ccommiit

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ document.addEventListener("DOMContentLoaded", function () {
         selectElement.value = "#";
     }
 });
+// script.js
+
+// Get the current date
+let currentDate = new Date();
+
+// Get the elements where you want to display the date
+let currentDateElement = document.getElementById("currentDate");
+
+// Format the date as desired (e.g., "Month Day, Year")
+let options = { year: 'numeric', month: 'long', day: 'numeric' };
+let formattedDate = currentDate.toLocaleDateString(undefined, options);
+
+// Set the formatted date as the content of the currentDate element
+currentDateElement.textContent = formattedDate;
+
